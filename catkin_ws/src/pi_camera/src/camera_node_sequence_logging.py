07f3e7b75c2b4157f252c798dd1d11f12535a954
Zac Ravichandran <ravicz@rpi.edu>
2017-10-26 19:20:09 -0500

    fixed bug in camera logging cb logig

--- catkin_ws/src/pi_camera/src/camera_node_sequence_logging.py
+++ catkin_ws/src/pi_camera/src/camera_node_sequence_logging.py
@@ -92,8 +92,9 @@ class CameraNode(object):
         self.update_framerate=False
  
     def startCapturing(self):
+        self.loop_complete = True
         rospy.loginfo("[%s] Start capturing." %(self.node_name))
-        while not self.is_shutdown and not rospy.is_shutdown():
+        while not self.is_shutdown and not rospy.is_shutdown() and self.loop_complete:
             rospy.loginfo("started startCapturing")
             gen =  self.grabAndPublish(self.stream,self.pub_img)
             try:
@@ -108,7 +109,6 @@ class CameraNode(object):
         rospy.loginfo("[%s] Capture Ended." %(self.node_name))
 
     def grabAndPublish(self,stream,publisher):
-        self.loop_complete = True
         rospy.loginfo("Started grabAndPublish")
         while not self.update_framerate and not self.is_shutdown and not rospy.is_shutdown() and self.loop_complete: 
             yield stream
