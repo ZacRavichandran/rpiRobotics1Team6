62828a3559c80f2d67a55b0691ac06f721c53eae
Zac Ravichandran <ravicz@rpi.edu>
2017-11-21 10:00:10 -0600

    added topic reading and publishing for obstacle detector node

--- catkin_ws/src/apriltags_ros/apriltags_ros/src/obstacle_detector_node.py
+++ catkin_ws/src/apriltags_ros/apriltags_ros/src/obstacle_detector_node.py
@@ -3,15 +3,27 @@ import rospkg
 import rospy
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image
+from duckietown_msgs.msg import AprilTagDetection 
+from duckietown_msgs.msg import AprilTagDetectionArray
 
 class ObstacleDetectorNode(object):
 	def __init__(self):
 		self.node_name = "obstacle_detector_node"
 		self.sub_image = rospy.Subscriber("image_rect", Image, self.cbImage, queue_size=1)
-		self.pub_visualize = rospy.Publisher("~tag_detections", PoseStamped, queue_size=1)
+		self.pub_visualize = rospy.Publisher("~tag_detections", AprilTagDetectionArray, queue_size=1)
 
 	def cbImage(self, image):
-		pass
+		placeholder_z = 10
+		placeholder_tag_id = 0
+
+		tag_detection_array = TagDetectionArray()
+		tag_pose = PoseStamped()
+		tag_pose.pose.position.z = placeholder_z
+		tag_detection = AprilTagDetection()
+		tag_detection.position = tag_pose
+		tag_detection.id = placeholder_tag_id
+		tag_detection_array.detections.append(tag_detection)
+		self.pub_visualize.publish(tag_detection_array)
 
 
 
